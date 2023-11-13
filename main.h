4082d90e87b8858300937eb49473a3d3474ada4a
Mus-aShi <mostafaaantri88@gmail.com>
2023-11-13 18:13:54 +0000

    2nd commit

---
+++ main.h
@@ -0,0 +1,117 @@
+#ifndef _PRINTF_H
+#define _PRINTF_H
+
+#include <stdargs.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+#define OUTPUT_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+#define NULL_STRING "(null)"
+
+#define PARAMS_INIT (0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+#define CONVERT_LOWERCASE  1
+#define CONVERT_UNSIGNED   2
+
+/**
+ * struct parameters - params struct
+ * @unsign: flag if unsigned value
+ *
+ * @plus_flag: on if plus flag specified
+ * @space_flag: on if space flag specified
+ * @hashtag_flag: on if hashtag flag spesified
+ * @zero_flag: on if zero flag specified
+ * @minus_flag: on if minus flag is specified
+ *
+ * @width: field width specified
+ * @precision: field precision specified
+ *
+ * @h_modifier: on if h_modifier is specified
+ * @l_modifier: on if l_modifier is specified
+ *
+ */
+typedef struct parameters
+{
+	unsigned int unsign		: 1;
+
+	unsigned int plus_flag		: 1;
+	unsigned int space_flag		: 1;
+	unsigned int hashtag_flag	: 1;
+	unsigned int zero_flag		: 1;
+	unsigned int minus_flag		: 1;
+
+	unsigned int width;
+	unsigned int precision;
+
+	unsigned int h_modifier		: 1;
+	unsigned int l_modifier		: 1;
+} params_t;
+
+/**
+ * struct specifier - struct token
+ *
+ * @specifier: format token
+ * @f: the function associated
+ */
+
+typedef struct specifier
+{
+	char *specifier;
+	int (*f)(va_list, params_t *);
+} specifier_t;
+
+/* print_functions.c */
+int print_char(va_list ap, params_t *params);
+int print_int(va_list ap, params_t *params);
+int print_str(va_list ap, params_t *params);
+int print_percent(va_list ap, params_t *params);
+int print_S(va_list ap, params_t *params);
+
+/* put.c */
+int _puts(char *str);
+int _putchar(int c);
+
+/* number.c */
+char *convert(long int num, int base, int flags, params_t *params);
+int print_unsigned(va_list ap, params_t *params);
+int print_address(va_list ap, params_t *params);
+
+/* convert_number.c */
+int print_hex(va_list ap, params_t *params);
+int print_HEX(va_list ap, params_t *params);
+int print_binary(va_list ap, params_t *params);
+int print_octal(va_list ap, params_t *params);
+
+/* specifier.c */
+int (*get_specifier(char *s))(va_list ap, params_t *params);
+int get_print_func(char *s, va_list ap, params_t *params);
+int get_flag(char *s, params_t *params);
+int get_modifier(char *s, params_t *params);
+char *get_width(char *s, params_t *params, va_list ap);
+
+/* print_number.c */
+int _isdigit(int c);
+int _strlen(char *);
+int print_number(char *str, params_t *params);
+int print_number_right_shift(char *str, params_t *params);
+int print_number_left_shift(char *str, params_t *params);
+
+/* simple_printers.c */
+int print_from_to(char *start, char *stop, char *except);
+int print_rev(va_list ap, params_t *params);
+int print_rot13(va_list ap, params_t *params);
+
+/* params.c */
+void init_percision(char *p, params_t *params, va_list ap);
+
+/* string_fields.c */
+char *get_percision(char *p, params_t *params, va_list ap);
+
+/* printf.c */
+int _printf(const char *format, ...);
+
+#endif
